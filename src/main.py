5f408b1388c5fdf35d92b84313e6c1d19c401525
roya parsa <99053997+rparsa49@users.noreply.github.com>
2024-06-26 17:30:02 -0400

    performed calibration of variables, plotted them. added excel from previous study

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from dect_processing import *
 from models import *
+import scipy.optimize as sp
 
 # Paths to DICOM files and circles data
 initial_dicom_path = '/Users/royaparsa/NYPC-DCT/images/Emptyphantoms/Gammexbody/CT.1.3.12.2.1107.5.1.4.83775.30000024050721561583000000063.dcm'
@@ -26,8 +28,16 @@ radii_ratios = [0.3, 0.6, 0.9]
 contour_image, mean_hu_values = draw_and_calculate_circles(
     new_image, saved_circles, radii_ratios)
 
-# SANITY CHECK: Attempt to calculate using truth equations for assumption of all inserts being water
+# Extract hu_low, hu_mid, hu_high for each insert
+hu_low = mean_hu_values[::3]
+hu_mid = mean_hu_values[1::3]
+hu_high = mean_hu_values[2::3]
 
+print("HU LOW: ", hu_low)
+print("HU MID: ", hu_mid)
+print("HU HIGH: ", hu_high)
+
+# SANITY CHECK: Attempt to calculate using truth equations for assumption of all inserts being water
 # Atomic weights of hydrogen and oxygen
 atomic_weight_H = 1.0079
 atomic_weight_O = 15.999
@@ -61,12 +71,99 @@ rho_e_true = rho_e_truth(rho_m, w_m, rho_w, w_w, Z, A)
 print(f"True rho_e for water: {rho_e_true}")
 
 # Calculate Z_eff
-n = 3.1 # Power law constant
-z_eff_true =  z_eff_truth(w_m, Z, A, n)
+n = 3.1  # Power law constant
+z_eff_true = z_eff_truth(w_m, Z, A, n)
 print(f"True Z_eff for water {z_eff_true}")
 
 ln_i_true = ln_i_truth(w_m, Z, A, I)
 print(f"True ln(I) for water {ln_i_true}")
+
+# Calibration process
+# Define functions to fit the models
+def fit_rho(x, a):
+    return rho_e_model(x[0], x[1], a)
+
+def fit_z(x, a):
+    return z_eff_model(x[0], x[1], x[2], a, n)
+
+# Prepare data for fitting
+x_rho = np.array([hu_low, hu_high])
+y_rho = np.array([rho_e_true] * len(hu_low))
+
+# Fit the rho_e model
+popt_rho, pcov_rho = sp.curve_fit(fit_rho, x_rho, y_rho)
+print(f"Fitted rho_e model parameters: {popt_rho}")
+
+# Calculate rho_e for Z_eff model
+rho_for_z = [rho_e_model(hu_low[i], hu_high[i], popt_rho[0])
+             for i in range(len(hu_low))]
+rho_for_z_arr = np.array(rho_for_z)
+
+# Prepare data for fitting Z_eff model
+x_z = np.array([hu_low, hu_high, rho_for_z_arr])
+y_z = np.array([z_eff_true] * len(hu_low))
+
+# Fit the Z_eff model
+popt_z, pcov_z = sp.curve_fit(fit_z, x_z, y_z)
+print(f"Fitted Z_eff model parameters: {popt_z}")
+
+# Calculate measured values for comparison
+rho_e_meas = [rho_e_model(hu_low[i], hu_high[i], popt_rho[0])
+              for i in range(len(hu_low))]
+z_eff_meas = [z_eff_model(
+    hu_low[i], hu_high[i], rho_e_meas[i], popt_z[0], n) for i in range(len(hu_low))]
+ln_i_meas = [ln_i_fit(z_eff_meas[i]) for i in range(len(hu_low))]
+
+# Calculate relative differences
+rho_e_diff = [(rho_e_meas[i] - rho_e_true) / rho_e_true *
+              100 for i in range(len(hu_low))]
+z_eff_diff = [(z_eff_meas[i] - z_eff_true) / z_eff_true *
+              100 for i in range(len(hu_low))]
+ln_i_diff = [(ln_i_meas[i] - ln_i_true) / ln_i_true *
+             100 for i in range(len(hu_low))]
+
+# Create arrays for true values with the same length as measured values
+rho_e_true_arr = np.full(len(hu_low), rho_e_true)
+z_eff_true_arr = np.full(len(hu_low), z_eff_true)
+ln_i_true_arr = np.full(len(hu_low), ln_i_true)
+
+# Plotting
+fig, ax = plt.subplots(2, 3, figsize=(18, 12))
+
+# Scatter plots of measured vs. true values
+ax[0, 0].scatter(rho_e_meas, rho_e_true_arr, c='g')
+ax[0, 0].plot(rho_e_true_arr, rho_e_true_arr, 'r--')
+ax[0, 0].set_xlabel('True rho_e')
+ax[0, 0].set_ylabel('Measured rho_e')
+
+ax[0, 1].scatter(z_eff_meas, z_eff_true_arr, c='g')
+ax[0, 1].plot(z_eff_true_arr, z_eff_true_arr, 'r--')
+ax[0, 1].set_xlabel('True Z_eff')
+ax[0, 1].set_ylabel('Measured Z_eff')
+
+ax[0, 2].scatter(ln_i_meas, ln_i_true_arr, c='g')
+ax[0, 2].plot(ln_i_true_arr, ln_i_true_arr, 'r--')
+ax[0, 2].set_xlabel('True ln(I)')
+ax[0, 2].set_ylabel('Measured ln(I)')
+
+# Scatter plots of relative differences
+ax[1, 0].scatter(rho_e_diff, rho_e_true_arr, c='g')
+ax[1, 0].plot([min(rho_e_true_arr), max(rho_e_true_arr)], [0, 0], 'r--')
+ax[1, 0].set_xlabel('True rho_e')
+ax[1, 0].set_ylabel('Relative difference (%)')
+
+ax[1, 1].scatter(z_eff_diff, z_eff_true_arr, c='g')
+ax[1, 1].plot([min(z_eff_true_arr), max(z_eff_true_arr)], [0, 0], 'r--')
+ax[1, 1].set_xlabel('True Z_eff')
+ax[1, 1].set_ylabel('Relative difference (%)')
+
+ax[1, 2].scatter(ln_i_diff, ln_i_true_arr, c='g')
+ax[1, 2].plot([min(ln_i_true_arr), max(ln_i_true_arr)], [0, 0], 'r--')
+ax[1, 2].set_xlabel('True ln(I)')
+ax[1, 2].set_ylabel('Relative difference (%)')
+
+plt.tight_layout()
+plt.show()
 
 # Display the result
 # display_image(contour_image, title='Mapped Areas of Interest On New Image')
--- src/models.py
+++ src/models.py
@@ -6,6 +6,63 @@ These method formulas are references from the following:
 https://github.com/NewYorkProtonCenter/dual-energy-ct/blob/main/dect_formula.py
 '''
 
+def beta_proton(ke):
+    """
+    Calculate the beta of proton with kinetic energy
+    Argument:
+        ke: kinetic energy of proton (MeV)
+    """
+    m0 = 938.272  # MeV/c^2
+    c = 299792458  # m/s
+    ene = m0 + ke
+    gamma = ene / m0
+    return 1 - (1 / (gamma * gamma))  # math.sqrt(1 - (1 / (gamma * gamma)))
+
+def rho_e_saito(hu1, hu2, alpha, a, b):
+    """
+    The electron density ratio (Phys. Med. Biol. 62 (2017) 7056).
+    Arguments:
+        hu1 (float): 1st CT-value in HU
+        hu2 (float): 2nd CT-value in HU
+        alpha (float): calibration constant
+        a (float): calibration constant
+        b (float): calibration constant
+    """
+    delta_hu = (1.0 + alpha) * hu2 - alpha * hu1
+    return a * delta_hu / 1000.0 + b
+
+def z_eff_saito(hu1, hu2, rho_e, n, beta, c, d):
+    """
+    The effective atomic number (Phys. Med. Biol. 62 (2017) 7056).
+    Arguments:
+        hu1 (float): 1st CT-value in HU
+        hu2 (float): 2nd CT-value in HU
+        rho_e (float): electron density ratio
+        n (float): power law
+        beta (float): calibration constant
+        c (float): calibration constant
+        d (float): calibration constant
+    """
+    delta_hu = (1.0 + beta) * hu2 - beta * hu1
+    inpow = (c * delta_hu / 1000.0 + d) / rho_e
+    if inpow.any() < 0:
+        print('in pow:', (c * delta_hu / 1000.0 + d) / rho_e)
+    return pow((c * delta_hu / 1000.0 + d) / rho_e, 1.0/n)
+
+def z_eff_model(hu_1, hu_2, r_e, d_e, n):
+    """
+    The effective atomic number (Phys. Med. Biol. 59 (2014) 83).
+    Arguments:
+        hu_1 (float): 1st CT-value in HU
+        hu_2 (float): 2nd CT-value in HU
+        r_e (float): the electron density ratio
+        d_e (float): calibration parameter
+        n: power
+    """
+    z_eff_w = 7.48  # 7.45  # (Phys. Med. Biol. 59 (2014) 83)
+    z_eff_w_n = pow(z_eff_w, n)
+    return pow((d_e * (hu_1 / 1000.0 + 1.0) + (z_eff_w_n - d_e) * (hu_2 / 1000.0 + 1.0)) / r_e, 1.0/n)
+
 def z_eff_truth(w_m, Z, A, n):
     """
     The effective atomic number (Phys. Med. Biol. 59 (2014) 83).
@@ -42,6 +99,32 @@ def rho_e_truth(rho_m, w_m, rho_w, w_w, Z, A):
         n_w = n_w + w_w[ele_i] * Z[ele_i] / A[ele_i]
     return rho_m * n_m / (rho_w * n_w)
 
+def rho_e_model(hu_1, hu_2, c_e):
+    """
+    The electron density ratio (Phys. Med. Biol. 59 (2014) 83).
+    Arguments:
+        c_e (float): calibration parameter
+        hu_1 (float): 1st CT-value in HU
+        hu_2 (float): 2nd CT-value in HU
+    """
+    return c_e * (hu_1 / 1000.0 + 1.0) + (1.0 - c_e) * (hu_2 / 1000.0 + 1.0)
+
+
+def ln_i_fit(z):
+    """
+    The ln of mean excitation potential (Phys. Med. Biol. 59 (2014) 83).
+    Arguments:
+        z (numpy.ndarray): effective atomic number
+    """
+    if z > 8.5:
+        a = 0.098
+        b = 3.376
+    else:
+        a = 0.125
+        b = 3.378
+
+    return a * z + b
+
 def ln_i_truth(w_m, Z, A, I):
     """
     The ln of mean excitation potential (truth).
@@ -59,3 +142,77 @@ def ln_i_truth(w_m, Z, A, I):
             Z[ele_i] / A[ele_i] * math.log(I[ele_i])
         denominator = denominator + w_m[ele_i] * Z[ele_i] / A[ele_i]
     return numerator / denominator
+
+
+def spr_w_truth(rho, ln_i_m, ln_i_w, beta2):
+    """
+    The stopping power ratio to water based on Bethe formula.
+    Arguments:
+        rho: electron density ratio
+        beta2: (v/c)^2 of the incident particle
+        ln_i_m: mean excitation potential
+        ln_i_w: mean excitation potential of water
+    """
+    return rho * (math.log(2 * 511000.0 * beta2 / (1 - beta2)) - beta2 - ln_i_m) / (math.log(2 * 511000.0 * beta2 / (1 - beta2)) - beta2 - ln_i_w)
+
+def sp_truth(za, ln_i_m, beta2):
+    """
+    The stopping power based on Bethe formula.
+    Arguments:
+        za: Z/A
+        beta2: (v/c)^2 of the incident particle
+        ln_i_m: mean excitation potential
+    """
+    return 0.307075*za/beta2*(math.log(2 * 511000.0 * beta2 / (1 - beta2)) - beta2 - ln_i_m)
+
+def sigma_christian0(v, z, a, m):
+    """
+    The relative cross-section to water for material with (w, z).
+    Arguments:
+        v: electron density fractions
+        z: atomic numbers
+        a: fitting par
+        b: fitting par
+        m: fitting par
+    """
+    sum_i = 0
+    for i in range(0, len(z)):
+        sum_i = sum_i + v[:, i] * pow(z[i], m)
+    b = (1 - a)/(0.2 + 0.8 * pow(8, m))
+    return a + b * sum_i
+
+def sigma_christian(v, z, a, m):
+    """
+    The relative cross-section to water for material with (w, z).
+    Arguments:
+        v: electron density fractions
+        z: atomic numbers
+        a: fitting par
+        m: fitting par
+    """
+    b = (1 - a)/(0.2 + 0.8 * pow(8, m))
+    sigma = np.zeros(len(v))
+    for j in range(0, len(v)):
+        sum_i = 0
+        for i in range(0, len(z)):
+            sum_i = sum_i + v[j][i] * pow(z[i], m)
+        sigma[j] = a + b * sum_i
+    return sigma
+
+def sigma_mono_ene(hu_l, hu_h, rho_e, alpha):
+    """
+    The relative cross-section to water for material with (w, z).
+    Arguments:
+        hu_l: HU low
+        hu_h: HU high
+        rho_e: electron density ratio
+        alpha: fitting parameter
+    """
+    sigma = np.zeros(len(rho_e))
+    for i in range(0, len(rho_e)):
+        mu_w_l_i = hu_l[i] / 1000.0 + 1.0
+        mu_w_h_i = hu_h[i] / 1000.0 + 1.0
+        sigma_w_l_i = mu_w_l_i / rho_e[i]
+        sigma_w_h_i = mu_w_h_i / rho_e[i]
+        sigma[i] = alpha * sigma_w_l_i + (1.0 - alpha) * sigma_w_h_i
+    return sigma
