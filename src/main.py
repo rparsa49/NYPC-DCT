575c3d29c920ed6ed7cb0b5916db1e560cf45b95
roya parsa <99053997+rparsa49@users.noreply.github.com>
2024-08-20 10:53:34 -0400

    Removed initial load of image to use static data, added material categorization, removed graphing

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-import os
-# import boto3
 import matplotlib.pyplot as plt
 import numpy as np
 from dect_processing import *
@@ -7,18 +5,11 @@ from models import *
 import scipy.optimize as sp
 
 # Paths to DICOM files and circles data
-initial_dicom_path = '/Users/royaparsa/NYPC-DCT/images/Emptyphantoms/Gammexbody/CT.1.3.12.2.1107.5.1.4.83775.30000024050721561583000000063.dcm'
-new_dicom_path = 'images/CT1.3.12.2.1107.5.1.4.83775.30000024051312040257200012307.dcm'
+new_dicom_path = "/Users/royaparsa/NYPC-DCT/images/CT1.3.12.2.1107.5.1.4.83775.30000024051312040257200012307.dcm"
 circles_data_path = 'circles_data.json'
 
-# Load and process initial DICOM image
-initial_image, initial_dicom_data = load_dicom_image(initial_dicom_path)
-
-# Process and save circles data
-process_and_save_circles(initial_image, circles_data_path)
-
 # Load new DICOM image
-new_image, _ = load_dicom_image(new_dicom_path)
+new_image, new_dicom_data = load_dicom_image(new_dicom_path)
 
 # Load saved circles data
 saved_circles = load_circles_data(circles_data_path)
@@ -26,9 +17,9 @@ saved_circles = load_circles_data(circles_data_path)
 # Define radii ratios
 radii_ratios = [0.3, 0.6, 0.9]
 
-# Draw circles and calculate HU values from mean pixel area in each circle
+# Draw circles and calculate HU values using modality LUT
 contour_image, mean_hu_values = draw_and_calculate_circles(
-    new_image, saved_circles, radii_ratios)
+    new_image, saved_circles, radii_ratios, new_dicom_data)
 
 # Display the result
 display_image(contour_image, title='Mapped Areas of Interest On New Image')
@@ -41,6 +32,23 @@ hu_high = mean_hu_values[2::3]
 print("HU LOW: ", hu_low)
 print("HU MID: ", hu_mid)
 print("HU HIGH: ", hu_high)
+
+materials_high = determine_materials(hu_high)
+materials_mid = determine_materials(hu_mid)
+materials_low = determine_materials(hu_low)
+
+# Display the results
+print("\nMaterials in larger AOI: ")
+for i, material in enumerate(materials_high):
+    print(f"Circle {i+1}: HU = {hu_high[i]}, Material = {material}")
+
+print("\nMaterials in medium AOI: ")
+for i, material in enumerate(materials_mid):
+    print(f"Circle {i+1}: HU = {hu_mid[i]}, Material = {material}")
+
+print("\nMaterials in smaller AOI: ")
+for i, material in enumerate(materials_low):
+    print(f"Circle {i+1}: HU = {hu_low[i]}, Material = {material}")
 
 # SANITY CHECK: Attempt to calculate using truth equations for assumption of all inserts being water
 # Atomic weights of hydrogen and oxygen
@@ -73,15 +81,15 @@ rho_m = 1.0
 
 # Calculate true rho_e
 rho_e_true = rho_e_truth(rho_m, w_m, rho_w, w_w, Z, A)
-print(f"True rho_e for water: {rho_e_true}")
+print(f"\nTrue rho_e for water: {rho_e_true}")
 
 # Calculate Z_eff
 n = 3.1  # Power law constant
 z_eff_true = z_eff_truth(w_m, Z, A, n)
-print(f"True Z_eff for water {z_eff_true}")
+print(f"\nTrue Z_eff for water {z_eff_true}")
 
 ln_i_true = ln_i_truth(w_m, Z, A, I)
-print(f"True ln(I) for water {ln_i_true}")
+print(f"\nTrue ln(I) for water {ln_i_true}")
 
 # Calibration process
 # Define functions to fit the models
@@ -97,7 +105,7 @@ y_rho = np.array([rho_e_true] * len(hu_low))
 
 # Fit the rho_e model
 popt_rho, pcov_rho = sp.curve_fit(fit_rho, x_rho, y_rho)
-print(f"Fitted rho_e model parameters: {popt_rho}")
+print(f"\nFitted rho_e model parameters: {popt_rho}")
 
 # Calculate rho_e for Z_eff model
 rho_for_z = [rho_e_model(hu_low[i], hu_high[i], popt_rho[0])
@@ -110,7 +118,7 @@ y_z = np.array([z_eff_true] * len(hu_low))
 
 # Fit the Z_eff model
 popt_z, pcov_z = sp.curve_fit(fit_z, x_z, y_z)
-print(f"Fitted Z_eff model parameters: {popt_z}")
+print(f"\nFitted Z_eff model parameters: {popt_z}")
 
 # Calculate measured values for comparison
 rho_e_meas = [rho_e_model(hu_low[i], hu_high[i], popt_rho[0])
@@ -126,61 +134,5 @@ spr_meas = [spr_w_truth(rho_e_meas[i], ln_i_meas[i], ln_i_true, beta2)
             for i in range(len(hu_low))]
 spr_true = spr_w_truth(rho_e_true, ln_i_true, ln_i_true, beta2)
 
-print(f"Stopping Power Ratio (SPR) for water: {spr_true}")
-print(f"Measured Stopping Power Ratios: {spr_meas}")
-
-# Calculate relative differences
-rho_e_diff = [(rho_e_meas[i] - rho_e_true) / rho_e_true *
-              100 for i in range(len(hu_low))]
-z_eff_diff = [(z_eff_meas[i] - z_eff_true) / z_eff_true *
-              100 for i in range(len(hu_low))]
-ln_i_diff = [(ln_i_meas[i] - ln_i_true) / ln_i_true *
-             100 for i in range(len(hu_low))]
-spr_diff = [(spr_meas[i] - spr_true) / spr_true *
-            100 for i in range(len(hu_low))]
-
-# Create arrays for true values with the same length as measured values
-rho_e_true_arr = np.full(len(hu_low), rho_e_true)
-z_eff_true_arr = np.full(len(hu_low), z_eff_true)
-ln_i_true_arr = np.full(len(hu_low), ln_i_true)
-spr_true_arr = np.full(len(hu_low), spr_true)
-
-# Plotting
-fig, ax = plt.subplots(2, 3, figsize=(18, 12))
-
-# Scatter plots of measured vs. true values
-ax[0, 0].scatter(rho_e_meas, rho_e_true_arr, c='g')
-ax[0, 0].plot(rho_e_true_arr, rho_e_true_arr, 'r--')
-ax[0, 0].set_xlabel('True rho_e')
-ax[0, 0].set_ylabel('Measured rho_e')
-
-ax[0, 1].scatter(z_eff_meas, z_eff_true_arr, c='g')
-ax[0, 1].plot(z_eff_true_arr, z_eff_true_arr, 'r--')
-ax[0, 1].set_xlabel('True Z_eff')
-ax[0, 1].set_ylabel('Measured Z_eff')
-
-ax[0, 2].scatter(ln_i_meas, ln_i_true_arr, c='g')
-ax[0, 2].plot(ln_i_true_arr, ln_i_true_arr, 'r--')
-ax[0, 2].set_xlabel('True ln(I)')
-ax[0, 2].set_ylabel('Measured ln(I)')
-
-# Scatter plots of relative differences
-ax[1, 0].scatter(rho_e_diff, rho_e_true_arr, c='g')
-ax[1, 0].plot([min(rho_e_true_arr), max(rho_e_true_arr)], [0, 0], 'r--')
-ax[1, 0].set_ylabel('True rho_e')
-ax[1, 0].set_xlabel('Relative difference (%)')
-
-ax[1, 1].scatter(z_eff_diff, z_eff_true_arr, c='g')
-ax[1, 1].plot([min(z_eff_true_arr), max(z_eff_true_arr)], [0, 0], 'r--')
-ax[1, 1].set_ylabel('True Z_eff')
-ax[1, 1].set_xlabel('Relative difference (%)')
-
-ax[1, 2].scatter(ln_i_diff, ln_i_true_arr, c='g')
-ax[1, 2].plot([min(ln_i_true_arr), max(ln_i_true_arr)], [0, 0], 'r--')
-ax[1, 2].set_ylabel('True ln(I)')
-ax[1, 2].set_xlabel('Relative difference (%)')
-
-# plt.tight_layout()
-# plt.show()
-
-
+print(f"\nStopping Power Ratio (SPR) for water: {spr_true}")
+print(f"\nMeasured Stopping Power Ratios: {spr_meas}")
